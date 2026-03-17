6c5f0485166887046b052ee23f93d3ab007ffe3b
Luaim <lmohammd4@gmail.com>
2026-03-18 00:38:11 +0800

    task 3: Validate transactions, persist records, and update user balances

--- src/main/java/com/jpmc/midascore/component/KafkaTransactionListener.java
+++ src/main/java/com/jpmc/midascore/component/KafkaTransactionListener.java
@@ -1,14 +1,56 @@
 package com.jpmc.midascore.component;
 
+import com.jpmc.midascore.entity.UserRecord;
 import com.jpmc.midascore.foundation.Transaction;
+import com.jpmc.midascore.foundation.TransactionRecord;
+import com.jpmc.midascore.repository.TransactionRecordRepository;
+import com.jpmc.midascore.repository.UserRepository;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 @Component
 public class KafkaTransactionListener {
 
+    private final UserRepository userRepository;
+    private final TransactionRecordRepository transactionRecordRepository;
+
+    public KafkaTransactionListener(UserRepository userRepository,
+                                    TransactionRecordRepository transactionRecordRepository) {
+        this.userRepository = userRepository;
+        this.transactionRecordRepository = transactionRecordRepository;
+    }
+
     @KafkaListener(topics = "${general.kafka-topic}")
     public void listen(Transaction transaction) {
-        System.out.println("Received amount: " + transaction.getAmount());
+
+        UserRecord sender = userRepository.findById(transaction.getSenderId());
+        UserRecord recipient = userRepository.findById(transaction.getRecipientId());
+
+        if (sender == null || recipient == null) {
+            return;
+        }
+
+        if (sender.getBalance() < transaction.getAmount()) {
+            return;
+        }
+
+        sender.setBalance(sender.getBalance() - transaction.getAmount());
+        recipient.setBalance(recipient.getBalance() + transaction.getAmount());
+
+        userRepository.save(sender);
+        userRepository.save(recipient);
+
+        TransactionRecord transactionRecord =
+                new TransactionRecord(sender, recipient, transaction.getAmount());
+
+        transactionRecordRepository.save(transactionRecord);
+
+        // Print Waldorf's balance
+        Iterable<UserRecord> users = userRepository.findAll();
+        for (UserRecord user : users) {
+            if ("waldorf".equals(user.getName())) {
+                System.out.println("WALDORF FINAL BALANCE: " + user.getBalance());
+            }
+        }
     }
 }
---
+++ src/main/java/com/jpmc/midascore/foundation/TransactionRecord.java
@@ -0,0 +1,61 @@
+package com.jpmc.midascore.foundation;
+
+import com.jpmc.midascore.entity.UserRecord;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class TransactionRecord {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private UserRecord sender;
+
+    @ManyToOne
+    private UserRecord recipient;
+
+    private float amount;
+
+    public TransactionRecord() {
+    }
+
+    public TransactionRecord(UserRecord sender, UserRecord recipient, float amount) {
+        this.sender = sender;
+        this.recipient = recipient;
+        this.amount = amount;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public UserRecord getSender() {
+        return sender;
+    }
+
+    public void setSender(UserRecord sender) {
+        this.sender = sender;
+    }
+
+    public UserRecord getRecipient() {
+        return recipient;
+    }
+
+    public void setRecipient(UserRecord recipient) {
+        this.recipient = recipient;
+    }
+
+    public float getAmount() {
+        return amount;
+    }
+
+    public void setAmount(float amount) {
+        this.amount = amount;
+    }
+}
